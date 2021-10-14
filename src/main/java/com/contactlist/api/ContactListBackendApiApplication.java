fdf547409cf2ff7d97cdd4389a39eff966f304d4
Juanjo Ortiz <juanjose.ortizgarcia@outlook.com>
2021-10-14 11:22:43 +0200

    base project and basic dependencies

---
+++ src/main/java/com/contactlist/api/ContactListBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.contactlist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contactlist/api/ContactListBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactlist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
