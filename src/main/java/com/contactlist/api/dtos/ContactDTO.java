b72811c6d119589705c013e6b75496746854ab8a
Juanjo <juanjose.ortizgarcia@outlook.com>
2021-11-16 13:05:01 +0100

    add dtos completed

---
+++ src/main/java/com/contactlist/api/dtos/ContactDTO.java
@@ -0,0 +1,11 @@
+package com.contactlist.api.dtos;
+
+public class ContactDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int mobile;
+    private String photo;
+}
---
+++ src/main/java/com/contactlist/api/dtos/UserDTO.java
@@ -0,0 +1,7 @@
+package com.contactlist.api.dtos;
+
+public class UserDTO {
+    private Long id;
+    private String userName;
+    private String password;
+}
